56f8f155fef8ef77fbdba3a0742a105644a80527
Sabir_Ullah <sabir.shenwari22@gmail.com>
2019-05-20 10:07:24 +0200

    sabir

--- src/components/admin/add/AddLaw.js
+++ src/components/admin/add/AddLaw.js
@@ -2,7 +2,34 @@ import React from "react";
 import BackButton from "../../core/admin/BackButton";
 
 class AddLaw extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      subTitle: "",
+      protect: Boolean,
+      commencment: Date,
+      resume: "",
+      fullText: "",
+      link: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange({ name, value }) {
+/*     console.info(value);
+ */    this.setState({
+    [name]: value
+  });
+  }
   render() {
+    const { title, subTitle, protect, commencement, resume, fullText, link } = this.state;
+    console.log('title', title);
+    /* console.log('subTitle', subTitle);
+    console.log('protect', protect);
+    console.log('commencement', commencement);
+    console.log('resume', resume);
+    console.log('fullText', fullText);
+    console.log('link', link); */
     return (
       <div>
         <BackButton />
@@ -19,6 +46,9 @@ class AddLaw extends React.Component {
                     className="form-control"
                     id="inlineFormInputGroupUsername2"
                     placeholder="Protège ou détruit"
+                    onChange={event =>
+                      this.handleChange({ name: "protect", value: event.target.value })
+                    }
                   />
                 </div>
                 <div className="input-group mb-2 mr-sm-2">
@@ -30,6 +60,9 @@ class AddLaw extends React.Component {
                     className="form-control"
                     id="inlineFormInputGroupUsername2"
                     placeholder="Titre"
+                    onChange={event =>
+                      this.handleChange({ name: "title", value: event.target.value })
+                    }
                   />
                 </div>
                 <div className="input-group mb-2 mr-sm-2">
@@ -41,6 +74,9 @@ class AddLaw extends React.Component {
                     className="form-control"
                     id="inlineFormInputGroupUsername2"
                     placeholder="Sous-Titre"
+                    onChange={event =>
+                      this.handleChange({ name: "subTitle", value: event.target.value })
+                    }
                   />
                 </div>
                 <div className="input-group mb-2 mr-sm-2">
@@ -52,6 +88,9 @@ class AddLaw extends React.Component {
                     className="form-control"
                     id="inlineFormInputGroupUsername2"
                     placeholder="Lien externe"
+                    onChange={event =>
+                      this.handleChange({ name: "link", value: event.target.value })
+                    }
                   />
                 </div>
                 <div className="input-group mb-2 mr-sm-2">
@@ -63,6 +102,9 @@ class AddLaw extends React.Component {
                     className="form-control"
                     id="inlineFormInputGroupUsername2"
                     placeholder="Date d'entréé en vigueur"
+                    onChange={event =>
+                      this.handleChange({ name: "commencement", value: event.target.value })
+                    }
                   />
                 </div>
                 <div className="input-group mb-2 mr-sm-2">
@@ -71,6 +113,9 @@ class AddLaw extends React.Component {
                     className="form-control pb-4"
                     id="inlineFormInputGroupUsername2"
                     placeholder="Résumé"
+                    onChange={event =>
+                      this.handleChange({ name: "resume", value: event.target.value })
+                    }
                   />
                 </div>
                 <div className="input-group mb-2 mr-sm-2">
@@ -80,11 +125,14 @@ class AddLaw extends React.Component {
                     placeholder="Texte complet"
                     id="inlineFormInputGroupUsername2"
                     rows="3"
+                    onChange={event =>
+                      this.handleChange({ name: " fullText", value: event.target.value })
+                    }
                   />
                 </div>
                 <div className="form-row mt-3">
                   <div className="col-lg-4 col-12">
-                    <select className="form-control">
+                    <select class="form-control" >
                       <option>Catégories</option>
                       <option>2</option>
                       <option>3</option>
