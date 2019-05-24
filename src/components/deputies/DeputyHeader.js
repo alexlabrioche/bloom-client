f992ea52677a64ec5e58d24396b650e0f1efa747
Alex Baker <alexbakerdeveloper@gmail.com>
2019-05-24 10:48:32 +0200

    deputy header bar added

---
+++ src/components/deputies/DeputyHeader.js
@@ -0,0 +1,153 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import { Link } from "react-router-dom";
+
+import Gauge from "../core/front/Gauge";
+import Config from "../../Config";
+import Global from "../../Global";
+
+const Container = styled.div`
+  ${props => props.isScrolled && css``}
+  .header {
+    background: white;
+    padding: 1rem;
+    border-bottom: 1px solid rgba(230, 230, 230, 1);
+    z-index: 10;
+    ${props =>
+      props.isScrolled &&
+      css`
+        padding-left: 3rem;
+        height: 5rem;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        box-shadow: 0px 3px 5px 0px rgba(230, 227, 230, 1);
+      `}
+  }
+  .header-img-container {
+    height: 14rem;
+    width: 16rem;
+    padding: 1rem;
+    overflow: auto;
+    ${props =>
+      props.isScrolled &&
+      css`
+        height: 3rem;
+        width: 6rem;
+        padding: 0;
+      `}
+  }
+  .header-img {
+    border-radius: 2px;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  .header-title {
+    font-size: 3rem;
+    font-weight: 300;
+    text-align: center;
+    color: ${Global.color.accent};
+    transition: 0.3s ease;
+    ${props =>
+      props.isScrolled &&
+      css`
+        font-size: 2rem;
+        margin-left: -40rem;
+      `}
+  }
+  .header-gauge-legend {
+    font-size: 14px;
+    text-align: center;
+    margin-top: -20px;
+  }
+  .scrolled {
+    ${props =>
+      props.isScrolled &&
+      css`
+        display: none;
+      `}
+  }
+`;
+
+class DeputyHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isScrolled: false
+    };
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+  handleScroll(event) {
+    const { isScrolled } = this.state;
+    if (window.scrollY > 120) {
+      !isScrolled &&
+        this.setState({
+          isScrolled: true
+        });
+    }
+    if (window.scrollY < 120) {
+      isScrolled &&
+        this.setState({
+          isScrolled: false
+        });
+    }
+  }
+  render() {
+    const { deputy, finalNote } = this.props;
+    const { isScrolled } = this.state;
+    return (
+      <Container isScrolled={isScrolled}>
+        <div className="header row">
+          <div className="offset-2 col-4 offset-lg-0 col-lg-3">
+            <div className="header-img-container ">
+              <img
+                className="header-img"
+                src={`${Config.server}/${deputy.picture}`}
+                alt={deputy.slug}
+              />
+            </div>
+          </div>
+          <div className="col-12 col-lg-6">
+            <div className="header-content">
+              <div className="header-title">{deputy.name}</div>
+              <p className="scrolled header-description">
+                Groupe au Parlement Européen :{" "}
+                <Link to={`/groupes/${deputy.group.slug}`}>
+                  {deputy.group.name}
+                </Link>
+              </p>
+              <p className="scrolled header-description">
+                Taux de présence aux séances de vote :{" "}
+                {deputy.participationRate}%
+              </p>
+              <p className="scrolled header-description">
+                Parti National :{" "}
+                <Link to={`/partis/${deputy.party.slug}`}>
+                  {deputy.party.name}
+                </Link>
+              </p>
+              <p className="scrolled header-description">
+                {deputy.description}
+              </p>
+            </div>
+          </div>
+          <div className="offset-3 offset-lg-0 col-3">
+            <div className="scrolled header-gauge">
+              <Gauge finalNote={finalNote} />
+              <p className="scrolled header-gauge-legend">
+                % de votes protecteur de l'océan
+              </p>
+            </div>
+          </div>
+        </div>
+      </Container>
+    );
+  }
+}
+
+export default DeputyHeader;
--- src/components/deputies/DeputyProfile.js
+++ src/components/deputies/DeputyProfile.js
@@ -1,12 +1,10 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 
 import Api from "../../utils/Api";
 import Algo100 from "../../utils/Algo100";
-import Config from "../../Config";
-import Gauge from "../core/front/Gauge";
 
+import DeputyHeader from "./DeputyHeader";
 import FlipCard from "./FlipCard";
 
 const Container = styled.div`
@@ -15,31 +13,9 @@ const Container = styled.div`
   background: white;
   border-radius: 4px;
   border: 1px solid rgba(230, 230, 230, 1);
-  .header {
-    padding-top: 1rem;
-    border-bottom: 1px solid rgba(230, 230, 230, 1);
-    box-shadow: 0px 3px 5px 0px rgba(230, 227, 230, 1);
-  }
-  .header-img-container {
-    border-radius: 4px;
-    height: 14rem;
-    width: 16rem;
-    background-color: lightblue;
-    overflow: auto;
-  }
-  .header-img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-  .header-title {
-    font-size: 3rem;
-    text-align: center;
-  }
-  .header-gauge-legend {
-    font-size: 14px;
-    text-align: center;
-    margin-top: -15px;
+  .main-content {
+    padding-top: 5rem;
+    padding-bottom: 15rem;
   }
 `;
 class DeputyProfile extends React.Component {
@@ -58,12 +34,19 @@ class DeputyProfile extends React.Component {
     const deputy = await Api.getDeputyBySlug(slug);
     const votes = await this.getVotesFromCurrentDeputy();
     const id = deputy._id;
-    const finalNote = await Algo100(id);
-    console.info("cmpDM finalNote", finalNote);
+    // const finalNote = await Algo100(id);
+    // console.info("cmpDM finalNote", finalNote);
     this.setState({
       deputy,
       votes,
-      isLoading: false,
+      isLoading: false
+      // finalNote
+    });
+    this.getFinalNote(id);
+  }
+  async getFinalNote(id) {
+    const finalNote = await Algo100(id);
+    this.setState({
       finalNote
     });
   }
@@ -104,42 +87,7 @@ class DeputyProfile extends React.Component {
     }
     return (
       <Container className="container">
-        <div className="header row">
-          <div className="offset-3 col-6 offset-md-0 col-md-4 col-lg-3">
-            <div className="header-img-container ">
-              <img
-                className="header-img"
-                src={`${Config.server}/${deputy.picture}`}
-                alt={deputy.slug}
-              />
-            </div>
-          </div>
-          <div className="header-content col-12 col-md-8 col-lg-6">
-            <h3 className="header-title">{deputy.name}</h3>
-            <p className="header-description">
-              Groupe au Parlement Européen :{" "}
-              <Link to={`/groupes/${deputy.group.slug}`}>
-                {deputy.group.name}
-              </Link>
-            </p>
-            {/* <p className="header-description">
-              Taux de présence aux séances de vote : {deputy.participationRate}%
-            </p> */}
-            <p className="header-description">
-              Parti National :{" "}
-              <Link to={`/partis/${deputy.party.slug}`}>
-                {deputy.party.name}
-              </Link>
-            </p>
-            <p className="header-description">{deputy.description}</p>
-          </div>
-          <div className="header-gauge offset-3 col-6 offset-md-0 col-md-4 col-lg-3">
-            <Gauge finalNote={finalNote} />
-            <p className="header-gauge-legend">
-              % de votes protecteur de l'océan
-            </p>
-          </div>
-        </div>
+        <DeputyHeader deputy={deputy} finalNote={finalNote} />
 
         <div className="main-content container">
           <div className="row">
