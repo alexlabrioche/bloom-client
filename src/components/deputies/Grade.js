922d5ff2d348649ccdba816c8c792b80f1bec043
Alex Baker <alexbakerdeveloper@gmail.com>
2019-06-27 10:12:41 +0200

    sorting options added

--- src/components/deputies/Grade.js
+++ src/components/deputies/Grade.js
@@ -1,44 +1,57 @@
 import React from "react";
-import Api from "../../utils/Api";
-import GetGrade from "../../utils/GetGrade";
 
 class Grade extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      deputiesGrade: []
-    };
-  }
-  async componentDidMount() {
-    const deputies = await Api.getDeputies();
-    const votes = await Api.getVotes();
-    const deputiesGrade = [];
-    deputies.forEach(deputy => {
-      const deputyGrade = GetGrade(deputy._id, votes, 100);
-      const deputyAndHisGrade = {
-        name: deputy.name,
-        id: deputy._id,
-        grade: deputyGrade
-      };
-      deputiesGrade.push(deputyAndHisGrade);
-    });
-    this.setState({
-      deputiesGrade
-    });
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     deputiesGrade: []
+  //   };
+  // }
+  // async componentDidMount() {
+  //   const deputies = await Api.getDeputies();
+  //   const votes = await Api.getVotes();
+  //   const deputiesGrade = [];
+  //   deputies.forEach(deputy => {
+  //     const deputyGrade = GetGrade(deputy._id, votes, 100);
+  //     const deputyAndHisGrade = {
+  //       name: deputy.name,
+  //       id: deputy._id,
+  //       grade: deputyGrade
+  //     };
+  //     deputiesGrade.push(deputyAndHisGrade);
+  //   });
+  //   this.setState({
+  //     deputiesGrade
+  //   });
+  // }
+  sortAscending = () => {
+    const { deputiesGrade } = this.state;
+    deputiesGrade.sort((a, b) => a - b);
+    this.setState({ deputiesGrade });
+  };
+
+  sortDescending = () => {
+    const { deputiesGrade } = this.state;
+    deputiesGrade.sort((a, b) => a - b).reverse();
+    this.setState({ deputiesGrade });
+  };
 
   render() {
-    const { deputiesGrade } = this.state;
-    // console.info("state", deputiesGrade);
+    const { deputies } = this.props;
+    console.info("grade", deputies);
     return (
       <div>
-        GRADE
+        {/* GRADE
         {deputiesGrade.map((deputy, index) => (
           <div>
             {index} {deputy.name} {deputy.grade}
           </div>
         ))}
-        {/* <button onClick={this.sortbyHighNote(deputiesGrade)}>Best grade</button> */}
+        <button onClick={() => this.sortHighGrade(deputiesGrade)}>
+          meilleures notes
+        </button> */}
+        <button onClick={this.sortAscending}>asc</button>
+        <button onClick={this.sortDescending}>desc</button>
       </div>
     );
   }
