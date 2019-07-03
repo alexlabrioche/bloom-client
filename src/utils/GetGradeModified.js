70f2c8c0bc45b122c9b16ac13f5b16eec2c64edb
Alex Baker <alexbakerdeveloper@gmail.com>
2019-07-03 16:26:18 +0200

    fixed algorithm bugs

--- src/utils/GetGradeModified.js
+++ src/utils/GetGradeModified.js
@@ -5,7 +5,7 @@ const GetGrade = (deputyId, votes, categories, laws) => {
   const doAbsenceVote = 0.5;
   const doNotVote = 1;
   const votesFromCurrentDeputy = votes.filter(vote => {
-    console.info("      ", vote);
+    console.info("      allVotes : ", vote);
     return vote.deputy._id === deputyId && vote;
   });
   console.info("Votes du député en cours :", votesFromCurrentDeputy);
@@ -14,14 +14,15 @@ const GetGrade = (deputyId, votes, categories, laws) => {
   let deputyPoints = 0;
   let numberOfVotes = 0;
   let totalOfVotes = 0;
+  let actionOfVote = 0;
   let arrayOfSubTotals = [];
 
   categories.forEach(category => {
     console.info("CATEGORY :", category.name);
     laws.forEach(law => {
+      totalOfVotes++;
+      numberOfVotes++;
       if (category._id === law.category) {
-        totalOfVotes++;
-        numberOfVotes++;
         console.info("  LAW :", law.name);
         votesFromCurrentDeputy.forEach(vote => {
           if (law._id === vote.law._id) {
@@ -34,6 +35,7 @@ const GetGrade = (deputyId, votes, categories, laws) => {
             ) {
               console.info("  J'ai bien voté !!");
               deputyPoints += doGoodVote;
+              actionOfVote++;
               console.info("    deputyPoints", deputyPoints);
             }
             if (
@@ -42,35 +44,57 @@ const GetGrade = (deputyId, votes, categories, laws) => {
             ) {
               console.info("  J'ai mal voté !!");
               deputyPoints += doBadVote;
+              actionOfVote++;
+
               console.info("    deputyPoints", deputyPoints);
             }
             if (vote.decision === "abstention") {
               console.info("  J'ai pas voté !!");
               deputyPoints += doNotVote;
+              actionOfVote++;
+
               console.info("    deputyPoints", deputyPoints);
             }
           }
         });
       }
     });
-    deputyPoints += doAbsenceVote * numberOfVotes;
-    console.info("    deputyPoints", deputyPoints);
-    const maxNote = numberOfVotes * 3;
+
+    const lawsFromCurrentCategory = laws.filter(law => {
+      return category._id === law.category && law;
+    });
+    const nbOfCurrentLaws = lawsFromCurrentCategory.length;
+    console.info("NB OF CURRENT LAWS", nbOfCurrentLaws);
+    const maxNote = nbOfCurrentLaws * 3;
     console.info("maxNote", maxNote);
-    const coefficient = scale / maxNote;
-    console.info("Coefficient du Texte en cours :", coefficient);
+    if (actionOfVote === 0) {
+      console.info("ICI !!!");
+      deputyPoints += doAbsenceVote * nbOfCurrentLaws;
+    }
+    let coefficient = scale / maxNote;
+    if (coefficient === Infinity) {
+      coefficient = 0;
+    }
+    console.info("    actionOfVote", actionOfVote);
+    console.info("    deputyPoints", deputyPoints);
+    console.info("    numberOfVotes", numberOfVotes);
+    console.info("    totalOfVotes", totalOfVotes);
+    console.info("    Coefficient du Texte en cours :", coefficient);
+
     const subTotal = deputyPoints * coefficient;
     console.info("subTotal :", subTotal);
-    arrayOfSubTotals.push(subTotal);
+    if (nbOfCurrentLaws !== 0) {
+      arrayOfSubTotals.push(subTotal);
+    }
     console.info("arrayOfSubTotals :", arrayOfSubTotals);
 
     deputyPoints = 0;
     numberOfVotes = 0;
+    actionOfVote = 0;
   });
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
-  const nbOfCategories = categories.length;
   const finalNote = Number(
-    (arrayOfSubTotals.reduce(reducer) / nbOfCategories).toFixed(0)
+    (arrayOfSubTotals.reduce(reducer) / arrayOfSubTotals.length).toFixed(0)
   );
   console.info("Nombres de votes du député", totalOfVotes);
   console.info("Note finale :", finalNote);
